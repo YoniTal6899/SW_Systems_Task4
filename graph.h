aa34ca612ad352e8ef476568d9a9c014767cb7a9
Yonatan Tal <89694679+YoniTal6899@users.noreply.github.com>
2022-01-09 19:54:48 +0200

    Add files via upload

--- graph.h
+++ graph.h
@@ -1,29 +1,39 @@
-#ifndef GRAPH_
-#define GRAPH_
+struct Edge{
+    int src;
+    int dest;
+    int w;
+    struct Edge *next;
+    struct Edge *prev;
+} typedef Edge;
 
-typedef struct GRAPH_NODE_ *pnode;;
+struct Node{
+    struct Node *next;
+    struct Node *prev;
+    int id;
+    Edge *edges_head;
+    int neig;
+} typedef Node;
 
-typedef struct edge_ {
-    int weight;
-    pnode endpoint;
-    struct edge_ *next;
-} edge, *pedge;
+typedef struct Graph {
+    int N;
+    Node *head;
+} Graph;
 
+struct GraphDijkstra
+{
+    int vertexNum;
+    int **edges;
+};
 
-typedef struct GRAPH_NODE_ {
-    int node_num;
-    int tag;
-    pnode parent;
-    pedge edges;
-    struct GRAPH_NODE_ *next;
-} node, *pnode;
-
-void build_graph_cmd(pnode *head);
-void insert_node_cmd(pnode *head);
-void delete_node_cmd(pnode *head);
-void printGraph_cmd(pnode head); //for self debug
-void deleteGraph_cmd(pnode* head);
-void shortsPath_cmd(pnode head);
-void TSP_cmd(pnode head);
-
-#endif
+int shortestPath(int src,int dest, Graph *gr);
+int Dijkstra(struct GraphDijkstra *graph, int src,int dest);
+int shortestPathDijkstra(int src,int dest, Graph *gr);
+void get_edges(int src_A, struct Graph *gr);
+Graph *build_graph(int);
+void set_edge(int, int, int , Graph *gr);
+Node *add_node(int, Graph *gr);
+void delete_node(int, Graph *gr);
+int shortsPath(int, int, struct Graph *gr);
+void printGraph(struct Graph *gr);
+void delete_gr(Graph*gr);
+void TSP(int, struct Graph *gr);
